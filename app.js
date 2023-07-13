35b7fb84e5a30d20f469fa37a760fca0847b97c1
Nguyen Lam Chi Thong <thong.nguyen@vus-etsc.edu.vn>
2023-07-13 22:53:08 +0700

    update code rerun for docker

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
+import express from 'express';
 const app = express()
-const port = 3005
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to NodeJS Server!')
